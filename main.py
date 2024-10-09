8422baada01b514b71ce11b6101ed601594da80a
Hope <s.hope.lee@samsung.com>
2024-10-09 20:06:05 +0900

    add celery task for deviding numbers.\Put the command as like $ poetry run celery -A main.celery worker --loglevel=info

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from celery import Celery
 
 app = FastAPI()
+
+celery = Celery(
+    __name__,
+    broker="redis://127.0.0.1:6379/0", # Can define these broker, backend value into seperate config file or pull them from environment file.
+    backend="redis://127.0.0.1:6379/0"
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@celery.task # Send a task to celery worker
+def divide(x, y):
+    """divide x by y after sleeping 5 seconds"""
+    import time
+    time.sleep(5)
+    return x / y
